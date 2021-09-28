973405a2caf0d9c439f5ce7326314f634dbf37fb
Conrad <conrad_bezuidenhout@mckinsey.com>
2021-09-28 16:06:55 +0800

    compile frontend

---
+++ node_graph/frontend/build/precache-manifest.56c96d32786411ad012bb294535f64f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89170b5bed6d06727f2935d967ab7c5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57b48de7af885fb87d5a",
+    "url": "./static/css/main.9c17bb36.chunk.css"
+  },
+  {
+    "revision": "998f3c1ba00318f55a16",
+    "url": "./static/js/2.be213b0a.chunk.js"
+  },
+  {
+    "revision": "08febb052d063625ed6a0408f05d2081",
+    "url": "./static/js/2.be213b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b48de7af885fb87d5a",
+    "url": "./static/js/main.e8c9c554.chunk.js"
+  },
+  {
+    "revision": "576fa9430d1b1c8d9439",
+    "url": "./static/js/runtime-main.ad44d3dc.js"
+  }
+]);
